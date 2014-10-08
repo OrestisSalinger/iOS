8e973e5abde3140cc9604f2d89bbf0c451141aa3
orestis_salinger <orestis@salingers.eu>
2014-10-08 10:34:47 +0200

    Adding json auth project.

---
+++ bitfountain/OAuth4Json/OAuth4Json/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  OAuth4Json
+//
+//  Created by Orestis Salinger on 06.10.14.
+//  Copyright (c) 2014 Orestis Salinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    let oauth_version = "1.0a"
+    let authorization_header = false
+    
+    
+    let settings = [
+        "client_id": "cb963bc8cdcc350e230e",
+        "client_secret": "c6bfe071646023eb133dc7cb84e7c5117084baaf",
+        "authorize_uri": "https://api.xing.com/v1/authorize",
+        "token_uri": "https://api.xing.com/v1/request_token",
+        "access_token_url": "https://api.xing.com/v1/access_token",
+    ]
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        let oauth = OAuth2CodeGrant(settings: settings)
+        
+        
+        oauth.onAuthorize = { parameters in
+            println("Did authorize with parameters: \(parameters)")
+        }
+        oauth.onFailure = { error in        // `error` is nil on cancel
+            if nil != error {
+                println("Authorization went wrong: \(error!.localizedDescription)")
+            }
+        }
+        let redir = "myapp://callback"        // don't forget to register this scheme
+        let scope = "profile email"
+        let web = oauth.authorizeEmbedded(redir, scope: scope, params: nil, from: self)
+        oauth.afterAuthorizeOrFailure = { wasFailure in
+            web.dismissViewControllerAnimated(true, completion: nil)
+        }
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- bitfountain/XingConnector/XingConnector/ViewController.swift
+++ bitfountain/XingConnector/XingConnector/ViewController.swift
@@ -11,22 +11,44 @@ import UIKit
 class ViewController: UIViewController {
 
 //    var client = CROAuth2Client(baseURL: "https://api.xing.com/v1/users/:Orestis_Salinger/visits")
-    var xingConnection = XingConnection()
-    var client:CROAuth2Client = CROAuth2Client(baseURL: "https://api.xing.com/")
+//    var xingConnection = XingConnection()
+//    var client:CROAuth2Client = CROAuth2Client(baseURL: "https://api.xing.com/")
+    let settings = [
+        "client_id":"my_swift_app",
+        "client_secret": "C7447242-A0CF-47C5-BAC7-B38BA91970A9",
+        "authorize_uri": "https://authorize.smartplatforms.org/authorize",
+        "token_uri":"https://authorize.smartplatforms.org/token"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        CROAuth2Client.clientWithPresentingController(self)
-
         
-        self.client.retrieveAuthToken({ (authToken) -> Void in
+        let oauth = OAuth2CodeGrant(settings: settings)
+       
         
-            if let optionnalAuthToken = authToken {
-                println("Received access token: " + authToken!)
-                self.xingConnection.connect(authToken!);
-
+        oauth.onAuthorize = { parameters in
+            println("Did authorize with parameters: \(parameters)")
+        }
+        oauth.onFailure = { error in        // `error` is nil on cancel
+            if nil != error {
+                println("Authorization went wrong: \(error!.localizedDescription)")
             }
+        }
         
-        })
+        
+        
+//        CROAuth2Client.clientWithPresentingController(self)
+//
+//        
+//        self.client.retrieveAuthToken({ (authToken) -> Void in
+//        
+//            if let optionnalAuthToken = authToken {
+//                println("Received access token: " + authToken!)
+//                self.xingConnection.connect(authToken!);
+//
+//            }
+//        
+//        })
         
         
     
